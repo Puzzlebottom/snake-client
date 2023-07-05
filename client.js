e8f1b73b2aee48a403037d7f7fa4a1e41c06d768
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-05 16:11:11 -0400

    adds full functionality

--- client.js
+++ client.js
@@ -14,9 +14,31 @@ const connect = function() {
   });
 
   conn.on("connect", () => {
+    process.stdin.setRawMode(true);
+    process.stdin.setEncoding('utf8');
+
+    let facing = "up";
+
+    process.stdin.on('data', (key) => {
+      const left = "\x1b[D";
+      const up = "\x1b[A";
+      const right = "\x1b[C";
+      const down = "\x1b[B";
+
+      if (key === up && facing !== "down") facing = "up";
+      if (key === down && facing !== "up") facing = "down";
+      if (key === left && facing !== "right") facing = "left";
+      if (key === right && facing !== "left") facing = "right";
+    });
 
     logToClient("Hissss! You're connected.");
-    logToServer(conn, "Name", "DMC");
+    logToServer(conn, "Name: DMC");
+    setInterval(() => moveSnake(conn, facing), 50);
+  });
+
+  conn.on("end", () => {
+    logToClient("Hissss! Connection ended.");
+    process.exit();
   });
 
   return conn;
@@ -26,8 +48,12 @@ const logToClient = (message) => {
   process.stdout.write(message + "\n");
 };
 
-const logToServer = (connection, header, body) => {
-  return connection.write(`${header}: ${body}`);
+const logToServer = (connection, message) => {
+  connection.write(message);
+};
+
+const moveSnake = (connection, direction) => {
+  logToServer(connection, `Move: ${direction}`);
 };
 
 module.exports = { connect };
